55757644da7b8e38a4deb1eb8c8aa2b5bbbd29a6
Kishor Rathva <kishor.rathva@rapidops.com>
2019-05-07 17:46:30 +0530

    remove submit button after submit

--- src/app/components/dates/dates.component.ts
+++ src/app/components/dates/dates.component.ts
@@ -1,7 +1,6 @@
 import { Component, OnInit } from '@angular/core';
-import { FormBuilder , FormControl, Validators, FormGroup, FormArray } from '@angular/forms';
+import { FormBuilder, FormControl, Validators, FormGroup, FormArray } from '@angular/forms';
 import { JsonPipe } from '@angular/common';
-
 
 @Component({
   selector: 'app-dates',
@@ -10,7 +9,6 @@ import { JsonPipe } from '@angular/common';
 })
 export class DatesComponent implements OnInit {
   constructor(public fb: FormBuilder) { }
-
 
   // Getter method to access formcontrols
 
@@ -23,9 +21,9 @@ export class DatesComponent implements OnInit {
   isSelectedFromMonth = false;
 
   // Selected values
-  selectedFromYear: any ;
+  selectedFromYear: any;
   selectedFromMonth: any;
-  selectedToYear: any ;
+  selectedToYear: any;
   selectedToMonth: any;
 
   // From - To
@@ -40,7 +38,7 @@ export class DatesComponent implements OnInit {
   toYrMonths: any[] = [];
   betweenMonths: any[] = [];
 
-  yearDifference: any ;
+  yearDifference: any;
 
   // final result
   generatedData: any[] = [];
@@ -51,61 +49,54 @@ export class DatesComponent implements OnInit {
     MonthlyData: this.fb.array([])
   });
 
-
   builderForm = this.fb.group({
     FromYear: ['', [Validators.required]],
     FromMonth: ['', [Validators.required]],
     ToYear: ['', [Validators.required]],
-    ToMonth: ['', [Validators.required]],
+    ToMonth: ['', [Validators.required]]
   });
 
   // generate forms
   genForms(): FormGroup {
-
-      return this.fb.group({
-        month: [``, Validators.required],
-        firstName: ['', Validators.required],
-        middleName: ['', Validators.required],
-        lastName: ['', Validators.required]
-      });
-
+    return this.fb.group({
+      month: [``, Validators.required],
+      firstName: ['', Validators.required],
+      middleName: ['', Validators.required],
+      lastName: ['', Validators.required]
+    });
   }
 
   // addforms for monthly data
   addMonthData(): void {
     const control = this.formDataGroup.get('MonthlyData');
-    for ( const i of this.formated) {
-      (control as FormArray).push(this.fb.group({
-        month: `${i}`,
-        firstName: '',
-        middleName: '',
-        lastName: ''
-       }));
+    for (const i of this.formated) {
+      (control as FormArray).push(
+        this.fb.group({
+          month: `${i}`,
+          firstName: '',
+          middleName: '',
+          lastName: ''
+        })
+      );
     }
-
-
 
     // for (const i in this.formated) {
     //   control.push(control.);
     // }
-    ( this.formDataGroup.get('MonthlyData') as FormArray).push(this.genForms());
+    (this.formDataGroup.get('MonthlyData') as FormArray).push(this.genForms());
     console.log(this.formDataGroup.get('MonthlyData'));
   }
 
   // Choose month using select dropdown
   changeFromMonth(e) {
     console.log('into the from month ....');
-    console.log(JSON.stringify((e.target.value).split(':')));
-    this.selectedFromMonth = this.months.indexOf(((e.target.value).split(':')[1]).trim()) ;
+    console.log(JSON.stringify(e.target.value.split(':')));
+    this.selectedFromMonth = this.months.indexOf(e.target.value.split(':')[1].trim());
     console.log(`selectedFromMonth:${this.selectedFromMonth}`);
     console.log(this.months);
     this.isSelectedFromMonth = true;
-
     // clear all the values that previuosly stored
-    this.generatedData = [];
-    this.betweenMonths = [];
-    this.fromYrMonths = [];
-    this.toYrMonths  = [];
+    this.clearner();
 
     // this.From.setValue(e.target.value, {
     //   onlySelf: true
@@ -113,53 +104,44 @@ export class DatesComponent implements OnInit {
   }
   changeToMonth(e) {
     console.log('......................');
-    console.log(JSON.stringify((e.target.value).split(':')));
-    this.selectedToMonth = this.months.indexOf(((e.target.value).split(':')[1]).trim()) ;
+    console.log(JSON.stringify(e.target.value.split(':')));
+    this.selectedToMonth = this.months.indexOf(e.target.value.split(':')[1].trim());
     console.log(`selectedToMonth:${this.selectedToMonth}`);
     // clear all the values that previuosly stored
-    this.generatedData = [];
-    this.betweenMonths = [];
-    this.fromYrMonths = [];
-    this.toYrMonths  = [];
+    this.clearner();
     // this.From.setValue(e.target.value, {
     //   onlySelf: true
     // });
   }
   changeFromYear(e) {
     console.log(e.target.value);
-    this.toYear = this.fromYear.slice((e.target.value).split(':')[0] - 1, this.fromYear.length);
-    console.log((e.target.value).split(':')[1]);
-    this.selectedFromYear = (e.target.value).split(':')[1];
+    this.toYear = this.fromYear.slice(e.target.value.split(':')[0] - 1, this.fromYear.length);
+    console.log(e.target.value.split(':')[1]);
+    this.selectedFromYear = e.target.value.split(':')[1];
     console.log(this.toYear);
 
     // clear all the values that previuosly stored
-    this.generatedData = [];
-    this.betweenMonths = [];
-    this.fromYrMonths = [];
-    this.toYrMonths  = [];
+    this.clearner();
     // this.From.setValue(e.target.value, {
     //   onlySelf: true
     // });
   }
   changeToYear(e) {
-    console.log(typeof(e.target.value));
-    console.log(JSON.stringify((e.target.value).split(':')));
+    console.log(typeof e.target.value);
+    console.log(JSON.stringify(e.target.value.split(':')));
     // console.log(`${this.selectedFromYear} == ${(e.target.value).split(':')[1]}`);
     // console.log(this.selectedFromYear === (e.target.value).split(':')[1]);
-    if (this.selectedFromYear === (e.target.value).split(':')[1] ) {
-        console.log(this.months.length);
-        this.toMonths = this.months.slice(+this.selectedFromMonth + 1, +this.months.length);
-        console.log(this.toMonths);
-     } else {
-       this.toMonths = this.months ;
-     }
-    this.selectedToYear = (e.target.value).split(':')[1];
+    if (this.selectedFromYear === e.target.value.split(':')[1]) {
+      console.log(this.months.length);
+      this.toMonths = this.months.slice(+this.selectedFromMonth + 1, +this.months.length);
+      console.log(this.toMonths);
+    } else {
+      this.toMonths = this.months;
+    }
+    this.selectedToYear = e.target.value.split(':')[1];
 
     // clear all the values that previuosly stored
-    this.generatedData = [];
-    this.betweenMonths = [];
-    this.fromYrMonths = [];
-    this.toYrMonths  = [];
+    this.clearner();
 
     // this.From.setValue(e.target.value, {
     //   onlySelf: true
@@ -176,7 +158,6 @@ export class DatesComponent implements OnInit {
       console.log(`selectedFromMonth-${this.selectedFromMonth}`);
       console.log(`selectedToMonth-${this.selectedToMonth}`);
       this.generatedData = this.months.slice(this.selectedFromMonth, +this.selectedToMonth + 1);
-
     } else {
       console.log(`selectedFromMonth-${this.selectedFromMonth}`);
       console.log(`selectedToMonth-${this.selectedToMonth}`);
@@ -186,65 +167,60 @@ export class DatesComponent implements OnInit {
       if (this.selectedToMonth === 0) {
         this.toYrMonths = ['JAN'];
       } else {
-        this.toYrMonths =  this.months.slice(0, +this.selectedToMonth + 1);
+        this.toYrMonths = this.months.slice(0, +this.selectedToMonth + 1);
       }
 
-
       for (let i = 0; i < this.yearDifference; i++) {
-        this.betweenMonths = (this.betweenMonths).concat(this.months);
+        this.betweenMonths = this.betweenMonths.concat(this.months);
         console.log(`at i = ${i}-${this.betweenMonths}`);
       }
       console.log(`fromYrMonths: ${this.fromYrMonths}`);
       console.log(`between months:${this.betweenMonths}`);
       console.log(`toYrMonths: ${this.toYrMonths}`);
-      this.generatedData = (this.generatedData).concat(this.fromYrMonths);
-      this.generatedData = (this.generatedData).concat(this.betweenMonths);
-      this.generatedData = (this.generatedData).concat(this.toYrMonths);
-
+      this.generatedData = this.generatedData.concat(this.fromYrMonths);
+      this.generatedData = this.generatedData.concat(this.betweenMonths);
+      this.generatedData = this.generatedData.concat(this.toYrMonths);
     }
     this.formatedData();
     console.log(this.formated);
     console.log(`generatedData : ${this.generatedData}`);
-
   }
   // Formated output
 
   formatedData(): void {
     const start = this.months[0];
-    let year = this.selectedFromYear ;
+    let year = this.selectedFromYear;
     let startFlagForJAN = 0;
     // check if genereted data has 'JAN' as first element
 
-    if ( this.generatedData[0] === start) {
-        startFlagForJAN = 1;
+    if (this.generatedData[0] === start) {
+      startFlagForJAN = 1;
     }
 
-    this.formated =  this.generatedData.map( i => {
-    if (i === start) {
-       if (startFlagForJAN) {
-         year -- ;
-         startFlagForJAN = 0;
-       }
-       year++;
-    }
-    return `${i}-${year}`;
-  });
-}
+    this.formated = this.generatedData.map((i) => {
+      if (i === start) {
+        if (startFlagForJAN) {
+          year--;
+          startFlagForJAN = 0;
+        }
+        year++;
+      }
+      return `${i}-${year}`;
+    });
+  }
 
   // Template Driven Form
   onSubmit() {
-
-      this.isSubmitted = true ;
-      this.yearDifference = this.selectedToYear - this.selectedFromYear - 1 ;
-      if (! this.builderForm.valid) {
-        return false;
-      } else {
-        console.log(JSON.stringify(this.builderForm.value));
-      }
-      this.generateData();
-      this.addMonthData();
+    this.isSubmitted = true;
+    this.yearDifference = this.selectedToYear - this.selectedFromYear - 1;
+    if (!this.builderForm.valid) {
+      return false;
+    } else {
+      console.log(JSON.stringify(this.builderForm.value));
     }
-
+    this.generateData();
+    this.addMonthData();
+  }
 
   // generate 100 years
 
@@ -254,13 +230,21 @@ export class DatesComponent implements OnInit {
     }
   }
 
- // Last output
- finalData(){
-   console.log(this.formDataGroup.value);
- }
+  // cleaner
+
+  clearner() {
+    // clear all the values that previuosly stored
+    this.generatedData = [];
+    this.betweenMonths = [];
+    this.fromYrMonths = [];
+    this.toYrMonths = [];
+  }
+  // Last output
+  finalData() {
+    console.log(this.formDataGroup.value);
+  }
 
   ngOnInit() {
     this.generateYear();
   }
-
 }
